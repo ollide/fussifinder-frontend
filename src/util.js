578a7835eb9959235cd96db6b677b24bf24091bc
Oliver Degener <hg.otothel@gmail.com>
2019-04-16 00:40:28 +0200

    util: throw WAIT error on 204 response, adds preventFocus as utility method

--- src/util.js
+++ src/util.js
@@ -2,10 +2,18 @@ export const handleFetchJsonResponse = (response) => {
     if (!response.ok) {
         throw Error(response.statusText);
     }
+    // data is being fetched on the server, this may take a couple of minutes
+    if (response.status === 204) {
+        throw Error('WAIT');
+    }
     const contentType = response.headers.get("content-type");
     if (contentType && contentType.indexOf("application/json") !== -1) {
         return response.json();
     } else {
         throw Error(`Invalid response type '${contentType}'`);
     }
+}
+
+export const preventFocus = (e) => {
+    e && e.preventDefault();
 }
